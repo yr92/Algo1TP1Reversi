4ca7962e3d5847209569f2531da6e779e360c373
yr92 <yoe.rosen@gmail.com>
2017-04-07 01:33:51 -0300

    Programa Principal
    Agregados registros y borrador de todos los procedimientos que van

--- Comienzo y Mostrar Matriz.pas
+++ Comienzo y Mostrar Matriz.pas
@@ -1,8 +1,42 @@
 program Reversi
 const COLUMNAS = 8;
       FILAS = 8;
-type tMatriz: array [1..FILAS,1..COLUMNAS] of char
+      FICHA_VACIA = 0;
+      FICHA_BLANCA = 1;
+      FICHA_NEGRA = 2;
+type
+  trJugador: record
+    nombre: char(20);
+    letra: char(1);
+    humano: boolean;
+    puntos: byte; //en vez contadores sueltos, mejor que quede guardado el puntaje en cada jugador
+  end
+  trCasilla: record
+    ficha: byte;
+  end
+  tMatriz: array [1..FILAS,1..COLUMNAS] of trCasilla
 var
+  vMatriz: tMatriz;
+  jugadorBlanco: trJugador;
+  jugadorNegro: trJugador;
+  jueganBlancas: boolean;//para ver de quien es el turno
+begin
+  //esto de aca abajo esta asi por ahora hasta que determ
+  //literalmente copiado del whatsapp, ya entraremos en mas detalles
+  InicializarVariables();
+  PedirNombreYFichas();
+  CrearTablero();
+  DibujarTablero();
+  ReiniciarTablero();
+  HayJugadaValida();//esta deberia ser funcion, por ahora la anoto asi
+  IndicarJugada();
+  HacerJugada();
+  PasarTurno();
+  MostrarPuntos();
+  CasilleroEsValido();
+  JuegoTerminado();
+end
+
 procedure mostrarMatriz (var matriz: tMatriz);
         var i,j: integer;
         begin
