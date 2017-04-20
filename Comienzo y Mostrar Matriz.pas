da71d923645bb997549721a62af89b1bca6d439e
yr92 <yoe.rosen@gmail.com>
2017-04-20 03:56:26 -0300

    agregadas direcciones

--- Comienzo y Mostrar Matriz.pas
+++ Comienzo y Mostrar Matriz.pas
@@ -1,13 +1,27 @@
-program Reversi
+program Reversi__;
 const COLUMNAS = 8;
       FILAS = 8;
       FICHA_VACIA = 0;
       FICHA_BLANCA = 1;
       FICHA_NEGRA = 2;
+      MAX_DIRECCIONES = 8;
+      DIR_ARRIBA = 1;
+      DIR_DERECHA_ARRIBA = 2;
+      DIR_DERECHA = 3;
+      DIR_DERECHA_ABAJO = 4;
+      DIR_ABAJO = 5;
+      DIR_IZQUIERDA_ABAJO = 6;
+      DIR_IZQUIERDA = 7;
+      DIR_IZQUIERDA_ARRIBA = 8;
+      ARRIBA = -1;
+      ABAJO = 1;
+      IZQUIERDA = -1;
+      DERECHA = 1;
+      NEUTRO = 0;
 type
   trJugador: record
-    nombre: string[20];
-    letra: char;
+    nombre: char(20);
+    letra: char(1);
     humano: boolean;
     puntos: byte; //en vez contadores sueltos, mejor que quede guardado el puntaje en cada jugador
     blancas: boolean; //para diferenciar un jugador de otro
@@ -21,6 +35,12 @@ type
     blancas: boolean; //para ver que jugador hizo la jugada
     valida: boolean
   end
+  trDireccion: record
+    dirX: byte;
+    dirY: byte;
+  end
+
+  tDirecciones: array[1..MAX_DIRECCIONES] of trDireccion
   tMatriz: array [1..FILAS,1..COLUMNAS] of trCasilla
 var
   vMatriz: tMatriz;
@@ -43,10 +63,30 @@ begin
        CasilleroEsValido();
        HacerJugada();
     end
-    MostrarPuntos();
+    CalcularYMostrarPuntos();
     PasarTurno();
     JuegoTerminado();
   until gameOver;
+end
+
+procedure inicializarDirecciones(var mDirecciones: tDirecciones);
+begin
+      mDirecciones[DIR_ARRIBA].dirX=NEUTRO;
+      mDirecciones[DIR_DERECHA_ARRIBA].dirX=ARRIBA;
+      mDirecciones[DIR_DERECHA].dirX=NEUTRO;
+      mDirecciones[DIR_DERECHA_ABAJO].dirX=DERECHA;
+      mDirecciones[DIR_ABAJO].dirX=NEUTRO;
+      mDirecciones[DIR_IZQUIERDA_ABAJO].dirX=IZQUIERDA;
+      mDirecciones[DIR_IZQUIERDA].dirX=IZQUIERDA;
+      mDirecciones[DIR_IZQUIERDA_ARRIBA].dirX=IZQUIERDA;
+      mDirecciones[DIR_ARRIBA].dirY=ARRIBA;
+      mDirecciones[DIR_DERECHA_ARRIBA].dirY=ARRIBA;
+      mDirecciones[DIR_DERECHA].dirY=NEUTRO;
+      mDirecciones[DIR_DERECHA_ABAJO].dirY=ABAJO;
+      mDirecciones[DIR_ABAJO].dirY=ABAJO;
+      mDirecciones[DIR_IZQUIERDA_ABAJO].dirY=ABAJO;
+      mDirecciones[DIR_IZQUIERDA].dirY=NEUTRO;
+      mDirecciones[DIR_IZQUIERDA_ARRIBA].dirY=ARRIBA;
 end
 
 procedure mostrarMatriz (var matriz: tMatriz);
@@ -59,5 +99,3 @@ procedure mostrarMatriz (var matriz: tMatriz);
 
               end;
         end;
-
-
