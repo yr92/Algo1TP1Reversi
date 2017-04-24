aa637bef9c56afc09f71d4c86f8107ec456c04e3
yr92 <yoe.rosen@gmail.com>
2017-04-24 05:27:36 -0300

    ahora usa constantes y registros

--- tableroInicial_2.0.pas
+++ tableroInicial_2.0.pas
@@ -3,6 +3,9 @@ program tableroInicial;
 const
   LIN_HORIZONTAL = '+---+---+---+---+---+---+---+---+';
   LIN_VERTICAL = '|';
+  TABLERO_HEADER = '    1   2   3   4   5   6   7   8';
+  MAX_FILASCOLUMNAS = 8;
+  MIN_FILASCOLUMNAS = 1;
   COLUMNAS = 8;
   FILAS = 8;
   FICHA_VACIA = 0;
@@ -26,10 +29,10 @@ function estaEnTablero(jugadaX, jugadaY: byte): boolean;
 var
   estaAdentro: boolean;
 begin
-  if ((jugadaX <= 8) and (jugadaX >= 1) and (jugadaY <= 8) and (jugadaY >= 1)) then
-   estaAdentro:= true
+  if ((jugadaX <= MAX_FILASCOLUMNAS) and (jugadaX >= MIN_FILASCOLUMNAS) and (jugadaY <= MAX_FILASCOLUMNAS) and (jugadaY >= MIN_FILASCOLUMNAS)) then
+       estaAdentro:= true
    else
-    estaAdentro:= false;
+       estaAdentro:= false;
   estaEnTablero:= estaAdentro;
 end;
 
@@ -58,14 +61,12 @@ begin
   InicializarMatriz(mMatriz);
 end;
 
-procedure HacerJugada(var mMatriz: tMatriz);
+procedure HacerJugada(var mMatriz: tMatriz; var mJugada: trJugada; var mJugador: trJugador);
 var
-  x,y:byte;
+  x:byte;
+  y:byte;
 begin
-  write('Ingrese la fila de su jugada: ');
-  readln(x);
-  write('Ingrese la columna de su jugada: ');
-  read(y);
+  IngresarJugada(mJugada, mJugador);
   while (not estaEnTablero(x,y)) do  //Hay que validar completamente, esto solo se fija que este en tablero
     begin
     writeln('Ingrese una jugada valida.'); writeln;
@@ -78,6 +79,45 @@ begin
   //Aca deberia ser con el jugador que le corresponda y bla bla
 end;
 
+procedure IngresarJugada(var mJugada: trJugada; var mJugador: trJugador);
+var
+    x,y:string;
+    valido: boolean;
+begin
+    IngresarCoordenada(mJugada, mJugador, 'fila');
+    IngresarCoordenada(mJugada, mJugador, 'columna');
+end;
+
+procedure IngresarCoordenada(var mJugada: trJugada; var mJugador: trJugador; filaOcolumna: string);
+var
+    dato: string;
+    valido: boolean;
+begin
+    repeat
+      write('Jugador ' + mJugador.nombre + ', ingrese la ' + filaOcolumna + ' de su jugada: ');
+      readln(dato);
+      if validarTexto(dato) = false then
+         writeln('Dato inválido. El valor ingresado debe ser un único número entre 1 y 8.');
+      else
+         valido := true;
+     until valido;
+     if filaOcolumna = 'fila' then
+          mJugada.X := strtoint(dato);
+     else
+          mJugada.Y := strtoint(dato);
+end;
+
+function validarTexto(mDato: string): boolean;
+var
+    valido: boolean;
+begin
+     if strlen(mDato) <> 1 or not isnumber (mDato) then
+        valido := false;
+     else
+         valido := true;
+     validarTexto := valido;
+end;
+
 procedure Puntuacion(var mMatriz: tMatriz);
 var
   i,j: byte;
@@ -85,8 +125,8 @@ var
 begin
   puntuacionBlancas:= 0;
   puntuacionNegras:= 0;
-  for i:=1 to FILAS do
-    for j:=1 to COLUMNAS do
+  for i:= MIN_FILASCOLUMNAS to MAX_FILASCOLUMNAS do
+    for j:= MIN_FILASCOLUMNAS to MAX_FILASCOLUMNAS do
       begin
       if (mMatriz[i,j] = 1) then
        inc(puntuacionBlancas);
@@ -96,20 +136,20 @@ begin
   write('       Blancas: ', puntuacionBlancas, ' - Negras: ', puntuacionNegras);
 end;
 
-procedure DibujarTablero (var mMatriz: tMatriz);
+procedure DibujarTablero (var mMatriz: tMatriz; var mJugadores: tJugadores);
 var
   i,j : byte;
 begin
-  writeln('    1   2   3   4   5   6   7   8');
+  writeln(TABLERO_HEADER);
   writeln('  ', LIN_HORIZONTAL);
-  for i:=1 to FILAS do
+  for i:=1 to MAX_FILASCOLUMNAS do
   begin
     write(i);
-    for j:=1 to COLUMNAS do
+    for j:=1 to MAX_FILASCOLUMNAS do
     begin
       write(' ',LIN_VERTICAL,' ');
-      if (mMatriz[i,j] <> 0) then
-       write(mMatriz[i,j])
+      if (mMatriz[i,j].ficha <> FICHA_VACIA) then
+       write(mJugadores[mMatriz[i,j].ficha].ficha)
       else
        write(' ');
     end;
